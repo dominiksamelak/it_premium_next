d5aaffbc033cd831b72d1deb639943fe08fc1a0c
dominiksamelak <123596496+dominiksamelak@users.noreply.github.com>
2025-06-14 01:40:34 +0200

    Update supabaseClient.js

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -2,14 +2,20 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseFunctionsUrl = process.env.NEXT_PUBLIC_SUPABASE_FUNCTIONS_URL;
 
 if (!supabaseUrl) {
   throw new Error("Missing environment variable: NEXT_PUBLIC_SUPABASE_URL");
 }
 if (!supabaseAnonKey) {
-  throw new Error(
-    "Missing environment variable: NEXT_PUBLIC_SUPABASE_ANON_KEY"
-  );
+  throw new Error("Missing environment variable: NEXT_PUBLIC_SUPABASE_ANON_KEY");
+}
+if (!supabaseFunctionsUrl) {
+  throw new Error("Missing environment variable: NEXT_PUBLIC_SUPABASE_FUNCTIONS_URL");
 }
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  functions: {
+    url: supabaseFunctionsUrl,
+  },
+});
